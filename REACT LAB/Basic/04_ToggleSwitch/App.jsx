6959bc1b318356d01ba3733b3d3df55b2b8cd154
Manav Delvadiya <24010101056@darshan.ac.in>
2025-09-09 11:16:32 +0530

    Add files via upload

---
+++ REACT LAB/Basic/04_ToggleSwitch/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ToggleSwitch } from "./Component/ToggleSwitch";
+import { Todo } from "./Component/Todo";
+
+function App() {
+
+  return <>
+  {/* <ToggleSwitch /> */}
+  <Todo />
+  </>;
+}
+
+export default App;
