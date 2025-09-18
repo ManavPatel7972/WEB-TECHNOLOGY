6643f3b947c0dfe703e60326198db4f548b5fa30
Manav Delvadiya <24010101056@darshan.ac.in>
2025-09-18 23:52:18 +0530

    Add files via upload

---
+++ REACT LAB/Lab-12/App12.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {UserContextProvider} from './Context/UserContextProvider';
+
+function App12() {
+  return (
+    <UserContextProvider />
+  )
+}
+
+export default App12
