9b56f8a1a58d0efb5a1879b0e6fbfde94f94296b
Manav Delvadiya <24010101056@darshan.ac.in>
2025-09-24 18:46:11 +0530

    Add files via upload

---
+++ REACT LAB/Basic/13_ZustandDemo/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { ApiFetchPost } from './Pages/ApiFetchPost'
+import {CountDemo} from './Pages/CountDemo'
+
+
+function App() {
+  
+
+  return (
+    <>
+  {/* <CountDemo/> */}
+  <ApiFetchPost/>
+    </>
+  )
+}
+
+export default App
